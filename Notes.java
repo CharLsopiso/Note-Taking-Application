2e12599105757bb1b8555c4f0834e18c37c441f2
CharLsopiso <117107047+CharLsopiso@users.noreply.github.com>
2022-11-01 01:18:13 +0800

    Create Notes.java

---
+++ Notes.java
@@ -0,0 +1,9 @@
+package Notes;
+
+public class Notes { 
+    public static void main(String[] args) {
+        notelist note = new notelist(); 
+        note.ExistingNotes(); 
+        note.MainMethod();    
+    }
+}
